7ed47dd4388d49fc12ea605e737be47f2cd5eb58
Simon <zilinskassimonas@gmail.com>
2026-08-12 16:20:38 +0200

    Update the FAQ for web search, energy class and GPU estimation

--- app/src/routes/product/[tab]/components/FAQ.svelte
+++ app/src/routes/product/[tab]/components/FAQ.svelte
@@ -7,7 +7,7 @@
     {
       id: 'usage',
       label: m['faq.usage.title'](),
-      qs: (['1', '2', '3', '4', '5', '6', '7'] as const).map((q) => ({
+      qs: (['1', '2', '3', '4', '5', '6'] as const).map((q) => ({
         title: m[`faq.usage.questions.${q}.title`](),
         desc: m[`faq.usage.questions.${q}.desc`]()
       }))
@@ -31,7 +31,7 @@
     {
       id: 'ecology',
       label: m['faq.ecology.title'](),
-      qs: (['1', '2', '3'] as const).map((q) => ({
+      qs: (['1', '2', '3', '4', '5'] as const).map((q) => ({
         title: m[`faq.ecology.questions.${q}.title`](),
         desc: m[`faq.ecology.questions.${q}.desc`]()
       }))
