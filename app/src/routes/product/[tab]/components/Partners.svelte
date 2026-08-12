23887040ac5a0734a57cfc423549f2e7b4c796ac
axolotle <nchesnais@mailoo.org>
2026-08-12 10:32:43 +0200

    feat: add product pages

---
+++ app/src/routes/product/[tab]/components/Partners.svelte
@@ -0,0 +1,112 @@
+<script>
+  import { Icon, Link } from '$components/dsfr'
+  import { m } from '$lib/i18n/messages'
+  import { CONTACT_URL } from '$lib/main'
+</script>
+
+<div class="gap-8 lg:grid-cols-2 grid">
+  <section class="cg-border cl-card">
+    <h2 class="fr-h6">
+      <Icon icon="i-ri-government-line" size="lg" class="text-primary me-1" />
+      {m['product.partners.institution.title']()}
+    </h2>
+
+    <div class="cl-logos">
+      <img
+        src="/orgs/cnnum.png"
+        alt="Conseil national du numérique"
+        title="Conseil national du numérique"
+        width="112px"
+      />
+      <img src="/orgs/etalab.svg" alt="Etalab" title="Etalab" width="240px" />
+      <img
+        src="/orgs/peren.svg"
+        alt="PEReN, Pôle d'expertise de la régulation numérique"
+        title="Pôle d'expertise de la régulation numérique (PEReN)"
+        width="240px"
+      />
+    </div>
+  </section>
+
+  <section class="cg-border cl-card">
+    <h2 class="fr-h6">
+      <Icon icon="i-ri-presentation-line" size="lg" class="text-yellow me-1" />
+      {m['product.partners.diffusion.title']()}
+    </h2>
+
+    <div class="cl-logos">
+      <img src="/orgs/cafe-ia.png" alt="Cafés de l'IA" title="Cafés IA" width="104px" />
+      <img
+        src="/orgs/latitudes.png"
+        alt="Latitudes"
+        title="Latitudes"
+        width="110px"
+        class="dark:invert"
+      />
+      <img src="/orgs/pix.svg" alt="Pix" title="Pix" width="60px" />
+      <img
+        src="/orgs/fresque-de-lia.png"
+        alt="Fresques de l'IA"
+        title="Fresques de l'IA"
+        width="94px"
+        class="dark:invert"
+      />
+    </div>
+
+    <p>{m['product.partners.diffusion.desc']()}</p>
+    <p><strong>{m['product.partners.diffusion.catch']()}</strong></p>
+
+    <Link
+      button
+      href="mailto:{CONTACT_URL}"
+      text={m['product.partners.diffusion.cta']()}
+      class="sm:w-auto! w-full!"
+    />
+  </section>
+
+  <section class="cg-border cl-card">
+    <h2 class="fr-h6">
+      <Icon icon="i-ri-chat-search-line" size="lg" class="text-secondary-text me-1" />
+      {m['product.partners.academy.title']()}
+    </h2>
+
+    <div class="cl-logos">
+      <img src="/orgs/inria.png" alt="Inria" title="Inria" width="139px" />
+    </div>
+
+    <p>{m['product.partners.academy.desc']()}</p>
+    <p><strong>{m['product.partners.academy.catch']()}</strong></p>
+
+    <Link
+      button
+      href="mailto:{CONTACT_URL}"
+      text={m['actions.contactUs']()}
+      class="sm:w-auto! w-full!"
+    />
+  </section>
+
+  <section class="cg-border cl-card">
+    <h2 class="fr-h6">
+      <Icon icon="i-ri-presentation-line" size="lg" class="text-green me-1" />
+      {m['product.partners.services.title']()}
+    </h2>
+
+    <div class="cl-logos">
+      <img src="/orgs/impactco2.png" alt="Impact CO2" title="Impact CO2" width="123px" />
+      <img src="/orgs/genai.png" alt="Gen AI Impact" title="Gen AI Impact" width="109px" />
+    </div>
+
+    <p>{m['product.partners.services.desc']()}</p>
+  </section>
+</div>
+
+<style lang="postcss">
+  @reference '$css/app.css';
+
+  .cl-card {
+    @apply bg-very-light-info p-4 md:p-8;
+  }
+  .cl-logos {
+    @apply my-4 gap-8 flex flex-wrap *:object-contain;
+  }
+</style>
